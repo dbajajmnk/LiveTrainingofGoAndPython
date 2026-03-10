1f7e1cce6129d5f014afbf0c50eb978aff5a431c
Deepak Kumar <deepakbajaj79@gmail.com>
2026-03-10 10:43:24 +0530

    Differences done

---
+++ Go/Day9/GoVsJava/Comparison.java
@@ -0,0 +1,25 @@
+public class Comparison {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+         int age=20;
+    if(age>=18)
+    {
+        System.out.println("You are elligble for Vote");
+    }
+
+    for(int i=0;i<10;i++)
+    {
+        System.out.println(i);
+    }
+    int j=0
+    while(j<10)
+    {
+        System.out.println(j);
+        j++;
+    }
+    }
+
+}
+
