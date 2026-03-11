f4bcef3557b66fb5caf83e7443255513dcdc94a3
Avichal Tiwari <avichaltiwari01@gmail.com>
2026-03-11 11:01:30 +0530

    Adding closure, varadic and multiple return value functions practice for day 10

---
+++ Practice Record Avi Day_10/Multiple_return/func_multi_return.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func division(a, b int) (int, string) {
+	if b == 0 {
+		return 80085, "Undefined result"
+	} else {
+		return a / b, "Defined result"
+	}
+}
+
+func main() {
+	a, b := 20, 10
+
+	result, validity := division(a, b)
+
+	// if validity != "Undefined result" {
+	// 	fmt.Println(result, validity)
+	// } else {
+	// 	fmt.Println(result, validity)
+	// }
+	fmt.Println(result, validity)
+}
---
+++ Practice Record Avi Day_10/Varadic_&_Closure/varadic.go
@@ -0,0 +1,16 @@
+// package main
+
+// import "fmt"
+
+// func sum(nums ...int) int {
+// 	total := 0
+// 	for _, num := range nums {
+// 		total += num
+// 	}
+// 	return total
+// }
+
+// func main() {
+// 	fmt.Println(sum(1, 2, 3))
+// 	fmt.Println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+// }
