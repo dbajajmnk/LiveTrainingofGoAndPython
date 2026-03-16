3596f56ac5f03bdb1c07e3f31ae0fd0d059c52c6
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-03-16 15:06:17 +0530

    Delete case implemented

--- Day13/FileHandling/main.go
+++ Day13/FileHandling/main.go
@@ -4,7 +4,7 @@ Interface :
 Discount
 increment
 decrement
-
+n("examples.txt")
 # Total for method
 
 # Product
@@ -20,13 +20,32 @@ import (
 
 func main() {
 
-	txtFile, err := os.Create("examples.txt")
+	// txtFile, err := os.Create("examples.txt")
+
+	// data, errReading := os.ReadFile("examples.txt")
+
+	// if errReading != nil {
+	// 	fmt.Println("Sorry unable to open file or Create File")
+	// } else {
+
+	// 	fmt.Println(string(data))
+	// }
+
+	AppendData("examples.txt", "Appending we are trying from Go")
+
+	deleteFileMsg, err := DeleteFile("examples.txt")
 	if err != nil {
-		fmt.Println("Sorry unable to open file or Create File")
+		fmt.Println("Error", err)
 	} else {
-		txtFile.WriteString("Hello Avichal Trim Question\n")
-		defer txtFile.Close()
+		fmt.Println(deleteFileMsg)
 	}
+
+	// if err != nil {
+	// 	fmt.Println("Sorry unable to open file or Create File")
+	// } else {
+	// 	txtFile.WriteString("Hello Avichal Trim Question\n")
+	// 	defer txtFile.Close()
+	// }
 
 }
 
@@ -39,4 +58,31 @@ func createTextFileAndWriteData(fileName string, extension string, data []string
 		defer txtFile.Close()
 	}
 	return *txtFile, err
+}
+func readFile(fileName string) (string, error) {
+	data, errReading := os.ReadFile(fileName)
+
+	return string(data), errReading
+}
+
+func AppendData(fileName string, data string) (os.File, error) {
+	txtfile, errReading := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0664)
+
+	if errReading != nil {
+		return *txtfile, errReading
+	}
+	txtfile.WriteString(data)
+	defer txtfile.Close()
+
+	return *txtfile, nil
+}
+
+func DeleteFile(fileName string) (string, error) {
+	errReading := os.Remove(fileName)
+
+	if errReading != nil {
+		return "Something went wrong", errReading
+	}
+
+	return "File Deleted Successfully", nil
 }
