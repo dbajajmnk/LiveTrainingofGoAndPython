12a1e818715d85647038cbe549ec7c1e7ef6b82c
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-03-30 17:24:11 +0530

    Revision of previous patterns for object creation

--- Day22/async await execution flow/RealUseCases/Loginflow.js
+++ Day22/async await execution flow/RealUseCases/Loginflow.js
@@ -1,3 +1,11 @@
+let user ={
+    name:"Deepak",
+    email:"deepak",
+    age:10,
+    password:"123",
+    phone:"9322333333333"
+}
+//Factory base
 function createUser(name,email,age,password,phone){
     return {name:name,
         email:email,
@@ -5,12 +13,43 @@ function createUser(name,email,age,password,phone){
         password:password,
         phone:phone
     }
-
 }
+//Constuctor base 
+function User(name,email,age,password,phone){
+    this.name=name;
+    this.email=email;
+    this.age=age;
+    this.password=password;
+    this.phone=phone;
+}
+
+function User(name,email,age,password,phone){
+    this.name=name;
+    this.email=email;
+    this.age=age;
+    this.password=password;
+    this.phone=phone;
+}
+
+class UserByClass{
+
+    constructor(name,email,age,password,phone){
+    this.name=name;
+    this.email=email;
+    this.age=age;
+    this.password=password;
+    this.phone=phone;
+    }
+}
+
+//Prototype base
+User.prototype.displayInfo = function(){
+    console.log(`Name: ${this.name}, Email: ${this.email}`);
+};
 let users=[createUser("Deepak","deepakbajaj79@gmail.com",10,"123","9322333333333"),
-    createUser("Avichal","avichal@gmail.com",25,"456","9322333333334"),
-    createUser("Rahul","rahul@gmail.com",30,"789","9322333333335"),
-    createUser("Priya","priya@gmail.com",28,"012","9322333333336")];
+    new User("Avichal","avichal@gmail.com",25,"456","9322333333334"),
+    new User("Rahul","rahul@gmail.com",30,"789","9322333333335"),
+    new UserByClass("Priya","priya@gmail.com",28,"012","9322333333336")];
 async function login(user){
         if (user.name=="Deepak" && user.password=="123"){
             return user;
@@ -37,12 +76,6 @@ async function loginAndDisplayUser(user){
 loginAndDisplayUser({name:"Deepak",password:"123"});
 
 
-function User(name,email,age,password,phone){
-    this.name=name;
-    this.email=email;
-    this.age=age;
-    this.password=password;
-    this.phone=phone;
-}
+
 let user1 =new User("Deepak","deepak@gmail.com",10,"123","9322333333333");
   
