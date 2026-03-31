c86ba1c2e461944919f6a9ed36e1d2e4f6fcf8ca
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-03-31 17:07:02 +0530

    fix mistakes

--- Day23/Browser performance bottlenecks/RealUseCases/ChangeThemeBasedonCard/main.js
+++ Day23/Browser performance bottlenecks/RealUseCases/ChangeThemeBasedonCard/main.js
@@ -1,9 +1,9 @@
-const them1=()={
+const them1=()=>{
     const children=[];
     for(let i=0;i<10;i++){
     const h1= document.createElement("h1");
     h1.textContent=i;
-    children.puhs(h1);
+    children.push(h1);
     }
     document.body.addChild([...children])
 
