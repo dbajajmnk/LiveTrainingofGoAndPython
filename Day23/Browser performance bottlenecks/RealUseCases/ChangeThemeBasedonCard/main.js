fc5bf52c51b149ff84f00c4f039ba822f1523f74
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-03-31 17:06:36 +0530

    Templtate code pushed

---
+++ Day23/Browser performance bottlenecks/RealUseCases/ChangeThemeBasedonCard/main.js
@@ -0,0 +1,16 @@
+const them1=()={
+    const children=[];
+    for(let i=0;i<10;i++){
+    const h1= document.createElement("h1");
+    h1.textContent=i;
+    children.puhs(h1);
+    }
+    document.body.addChild([...children])
+
+}
+const them2=()=>{
+
+}
+const them3=()=>{
+
+}
