45668bc7a51ea9d1d47f249a292b3baaac4e53b8
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-03-31 17:21:14 +0530

    Implemenation Done

--- Day23/Browser performance bottlenecks/RealUseCases/ChangeThemeBasedonCard/main.js
+++ Day23/Browser performance bottlenecks/RealUseCases/ChangeThemeBasedonCard/main.js
@@ -1,16 +1,22 @@
-const them1=()=>{
+const theme1=(elementname)=>{
     const children=[];
     for(let i=0;i<10;i++){
-    const h1= document.createElement("h1");
-    h1.textContent=i;
+    const h1= document.createElement(elementname);
+    h1.textContent="Content"+i;
     children.push(h1);
     }
-    document.body.addChild([...children])
+    document.body.appendChild(...children);
 
 }
-const them2=()=>{
+
+function changeTheme(choice){
+    //document.body.innerHtml="";
+    switch(choice){
+        case 1: theme1("h1"); break;
+        case 2: theme1("p"); break;
+        default: theme1("del"); break;
+    }
 
 }
-const them3=()=>{
+changeTheme(3);
 
-}
