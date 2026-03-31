a5b7cadfaa53f0127394f6cc294d667ca0bd9f11
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-03-31 15:13:33 +0530

    Working on Reflow and Repaint

---
+++ Day23/Reflow and repaint/Practical/main.js
@@ -0,0 +1,37 @@
+// const box = document.getElementById("box");
+
+// for(let i=100;i<1200;i+=100){
+//     box.style.width=i;
+//     box.style.height=i;
+// }
+
+// 1. Create one example that triggers repaint only
+// 2. Create one example that triggers reflow
+// 3. Add an element dynamically to a list
+// 4. Change an element’s width and observe effect
+// 5. Compare animation using `left` and `transform`
+
+const repaintOnly=()=>{
+
+    document.body.style.backgroundColor = "lightblue";
+}
+const reflowTrigger=()=>{
+    const box = document.getElementById("box");
+    box.style.width = "200px";
+}
+
+const addElements=(type,elements)=>{
+    const list=document.createElement(type);
+    for(let i=0;i<elements.length;i++){
+        const listItem=document.createElement("li");
+        listItem.textContent = elements[i];
+        list.appendChild(listItem);
+    }
+    document.body.appendChild(list);
+    
+
+
+}
+repaintOnly();
+reflowTrigger();
+addElements("ol",["Abhi","Raj","Kusuma","Lidiya","Piyush","Navya","Rohit"])
--- Day23/Reflow and repaint/main.js
+++
@@ -1,6 +0,0 @@
-const box = document.getElementById("box");
-
-for(let i=100;i<1200;i+=100){
-    box.style.width=i;
-    box.style.height=i;
-}
