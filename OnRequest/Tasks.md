277644868069db34f79a9a668c1bc4a574da2bd0
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-04-01 17:21:42 +0530

    Basic page structure ready

---
+++ OnRequest/Tasks.md
@@ -0,0 +1,13 @@
+1. Design our pages
+2. Implement features 
+   1. Able to enter prompt (Empty )
+   2. Perform sequential promoting till level 5 only
+   3. Retry (3)
+   4. Copy
+   5. Share ()
+   6. Like 
+   7. Dislike
+3. Technology stack
+   1. Front end React js + JavaScrip
+   2. Go Backend
+   3. Testing (Automation PlayWriter)
---
+++ OnRequest/apiproject/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [prompt, setPrompt] = useState('')
+  const [validationError, setValidationError] = useState('');
+
+  const handleInput = (e) =>{ 
+    setPrompt(e.target.value);
+    if(prompt.length >5) {
+      setValidationError("");
+    } 
+
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(prompt);
+    if(prompt.length < 5) {
+      setValidationError('Prompt must be at least 5 characters long');
+      setPrompt('');
+    }
+    else{
+      setValidationError('');
+      callApi('http://localhost:5000/api/generate', {prompt});
+      }
+  }
+  const callApi = (url,data)=>{
+      console.log("Calling API with data: ",url, data);
+  }
+
+  return (
+    <>
+    <h1>Welcome to Our Team app</h1>
+     <form onSubmit={onSubmit}>
+      <input type="text" name="userinput" placeholder="What is in your mind?" value={prompt} onChange={handleInput}/>
+      {validationError ? <h2>{validationError}</h2> : null}
+      <button type="submit" disabled={validationError}>Submit</button>
+     </form>
+    </>
+  )
+}
+
+export default App
