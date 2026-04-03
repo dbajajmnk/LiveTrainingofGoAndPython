fffffb9a850d5a303846bfe5b697523457d10fa9
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-04-03 16:26:08 +0530

    Sample code for Team review.

---
+++ Day26/Modular frontend architecture/frontarchitecture/src/utils/BasicsofjavaScript.js
@@ -0,0 +1,26 @@
+function Test(a,b){
+    return a+b;
+}
+
+const calulate=(a,b)=>{
+    return a*b;
+}
+
+let result = calulate(10,50);
+
+const pi=9.10;
+
+let numbers = [10,20,"Deepak",true,""];
+
+
+function user(name,age){
+    this.name=name;
+    this.age=age;
+}
+
+function calculateTax(payableAmount,taxPercentage){
+
+    return (payableAmount+10)*40+taxPercentage;
+}
+
+
